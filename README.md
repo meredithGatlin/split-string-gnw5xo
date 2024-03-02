82c9431260e8ef4698fba54b6ea3da6f3d11d943
meredithGatlin <159324773+meredithGatlin@users.noreply.github.com>
2024-03-01 17:07:35 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# split-string-gnw5xo
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/meredithGatlin/split-string-gnw5xo)
---
+++ index.js
@@ -0,0 +1,21 @@
+const prompCreator = require('prompt-sync');
+
+const prompt = prompCreator();
+
+// DO NOT CHANGE THE LINE BELOW.
+const str = prompt('Enter a string:');
+
+/**
+ * Create an array of substrings given a delimiter.
+ * For example, if str = 'Hello World!' and delimiter = ' ', it should return ['Hello', 'World!']
+ */
+let str = 'Hello World';
+let sep = str.split(' ', 3);
+let result = sep;
+
+// DO NOT CHANGE THE LINES BELOW.
+console.log(result);
+
+module.exports = {
+  result,
+};
